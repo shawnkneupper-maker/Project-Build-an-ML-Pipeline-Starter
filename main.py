ce2b0be9f0e466c31e8422a4465d808b09d09c4b
shawnkneupper-maker <shawnkneupper@gmail.com>
2026-02-26 17:52:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -50,15 +50,34 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+        _ = mlflow.run(
+            f"{config['main']['components_repository']}/basic_cleaning",
+            "main",
+            env_manager="conda",
+            parameters={
+            "input_artifact": "sample.csv:latest",
+            "output_artifact": "clean_sample.csv",
+            "output_type": "clean_sample",
+            "output_description": "Cleaned dataset",
+            "min_price": config["etl"]["min_price"],
+            "max_price": config["etl"]["max_price"],
+            },
+        )
             pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+            f"{config['main']['components_repository']}/data_check",
+            "main",
+            env_manager="conda",
+            parameters={
+            "csv": "clean_sample.csv:latest",
+            "ref": "clean_sample.csv:reference",
+            "kl_threshold": config["data_check"]["kl_threshold"],
+            "min_price": config["etl"]["min_price"],
+            "max_price": config["etl"]["max_price"],
+            },
+        )
             pass
 
         if "data_split" in active_steps:
