e0c9f192965df7f55ff388009242b38c9d678b90
shawnkneupper-maker <shawnkneupper@gmail.com>
2026-03-07 09:01:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,119 +7,103 @@ import wandb
 import hydra
 from omegaconf import DictConfig
 
-_steps = [
-    "download",
-    "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
-]
-
-
-# This automatically reads in the configuration
-@hydra.main(version_base=None, config_name='config', config_path='.')
-def go(config: DictConfig):
-
-    # Setup the wandb experiment. All runs will be grouped under this name
-    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
-    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+@hydra.main(config_name="config")
+def go(config):
 
     # Steps to execute
-    steps_par = config['main']['steps']
-    active_steps = steps_par.split(",") if steps_par != "all" else _steps
+    steps_to_execute = config["main"]["steps"].split(",")
 
-    # Move to a temporary directory
-    with tempfile.TemporaryDirectory() as tmp_dir:
+    # repository containing components
+    components_repository = config["main"]["components_repository"]
 
-        if "download" in active_steps:
-            # Download file and load in W&B
-            _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
-                "main",
-                env_manager="conda",
-                parameters={
-                    "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
-                },
-            )
+    # Random forest config file
+    rf_config = os.path.abspath(config["modeling"]["random_forest_config"])
 
-        if "basic_cleaning" in active_steps:
+    # -------------------------------------------------------
+    # DOWNLOAD STEP
+    # -------------------------------------------------------
+    if "download" in steps_to_execute:
+
         _ = mlflow.run(
-            f"{config['main']['components_repository']}/basic_cleaning",
+            f"{components_repository}/download",
             "main",
-            env_manager="conda",
             parameters={
-            "input_artifact": "sample.csv:latest",
-            "output_artifact": "clean_sample.csv",
-            "output_type": "clean_sample",
-            "output_description": "Cleaned dataset",
-            "min_price": config["etl"]["min_price"],
-            "max_price": config["etl"]["max_price"],
+                "file_url": config["data"]["file_url"],
+                "artifact_name": config["data"]["artifact_name"],
+                "artifact_type": config["data"]["artifact_type"],
+                "artifact_description": config["data"]["artifact_description"],
             },
         )
-            pass
 
-        if "data_check" in active_steps:
-            _ = mlflow.run(
-            f"{config['main']['components_repository']}/data_check",
+    # -------------------------------------------------------
+    # BASIC CLEANING STEP
+    # -------------------------------------------------------
+    if "basic_cleaning" in steps_to_execute:
+
+        _ = mlflow.run(
+            f"{components_repository}/basic_cleaning",
             "main",
-            env_manager="conda",
             parameters={
-            "csv": "clean_sample.csv:latest",
-            "ref": "clean_sample.csv:reference",
-            "kl_threshold": config["data_check"]["kl_threshold"],
-            "min_price": config["etl"]["min_price"],
-            "max_price": config["etl"]["max_price"],
+                "input_artifact": "sample.csv:latest",
+                "output_artifact": config["data"]["cleaned_artifact"],
+                "output_type": config["data"]["cleaned_artifact_type"],
+                "output_description": config["data"]["cleaned_artifact_description"],
             },
         )
-            pass
 
-        if "data_split" in active_steps:
-            ##################
-          _ = mlflow.run(
-            f"{config['main']['components_repository']}/train_val_test_split",
+    # -------------------------------------------------------
+    # DATA CHECK STEP
+    # -------------------------------------------------------
+    if "data_check" in steps_to_execute:
+
+        _ = mlflow.run(
+            f"{components_repository}/data_check",
             "main",
-            env_manager="conda",
-         parameters={
-            "input_artifact": "clean_sample.csv:latest",
-            "artifact_root": "data_split",
-            "test_size": config["modeling"]["test_size"],
-            "random_seed": config["modeling"]["random_seed"],
-            "stratify_by": config["modeling"]["stratify_by"],
+            parameters={
+                "csv": "clean_sample.csv:latest",
+                "ref": config["data"]["reference_dataset"],
+                "kl_threshold": config["data"]["kl_threshold"],
             },
         )
-            ##################
-            pass
 
-        if "train_random_forest" in active_steps:
+    # -------------------------------------------------------
+    # DATA SPLIT STEP
+    # -------------------------------------------------------
+    if "data_split" in steps_to_execute:
 
-            # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+        _ = mlflow.run(
+            f"{components_repository}/train_val_test_split",
+            "main",
+            parameters={
+                "input_artifact": "clean_sample.csv:latest",
+                "test_size": config["modeling"]["test_size"],
+                "random_seed": config["modeling"]["random_seed"],
+                "stratify_by": config["modeling"]["stratify_by"],
+            },
+        )
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
+    # -------------------------------------------------------
+    # TRAIN RANDOM FOREST STEP
+    # -------------------------------------------------------
+    if "train_random_forest" in steps_to_execute:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
-
-        if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+        _ = mlflow.run(
+            os.path.join(
+                hydra.utils.get_original_cwd(),
+                "src",
+                "train_random_forest",
+            ),
+            "main",
+            parameters={
+                "trainval_artifact": "trainval_data.csv:latest",
+                "output_artifact": "random_forest_export",
+                "rf_config": rf_config,
+                "random_seed": config["modeling"]["random_seed"],
+                "val_size": config["modeling"]["val_size"],
+                "stratify_by": config["modeling"]["stratify_by"],
+                "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+            },
+        )
 
 
 if __name__ == "__main__":
