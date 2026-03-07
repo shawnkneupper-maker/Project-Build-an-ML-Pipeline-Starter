c6e5eb03c8b03e4a5852b902fce3fdcc428cc56d
shawnkneupper-maker <shawnkneupper@gmail.com>
2026-03-07 10:31:49 -0600

    Update run.py

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -1,48 +1,36 @@
 #!/usr/bin/env python
 """
-This script download a URL to a local destination
+Download a file and upload it as W&B artifact
 """
 import argparse
 import logging
 import os
-
 import wandb
-
 from wandb_utils.log_artifact import log_artifact
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
-logger = logging.getLogger()
-
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def go(args):
-
     run = wandb.init(job_type="download_file")
     run.config.update(args)
 
-    logger.info(f"Returning sample {args.sample}")
-    logger.info(f"Uploading {args.artifact_name} to Weights & Biases")
+    local_path = os.path.join("data", args.sample)
+    logger.info(f"Uploading {args.artifact_name} to W&B from {local_path}")
+
     log_artifact(
         args.artifact_name,
         args.artifact_type,
         args.artifact_description,
-        os.path.join("data", args.sample),
-        run,
+        local_path,
+        run
     )
-
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download URL to a local destination")
-
-    parser.add_argument("--sample", type=str, help="Name of the sample to download")
-
-    parser.add_argument("--artifact_name", type=str, help="Name for the output artifact")
-
-    parser.add_argument("--artifact_type", type=str, help="Output artifact type.")
-
-    parser.add_argument(
-        "--artifact_description", type=str, help="A brief description of this artifact"
-    )
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sample", type=str, required=True)
+    parser.add_argument("--artifact_name", type=str, required=True)
+    parser.add_argument("--artifact_type", type=str, required=True)
+    parser.add_argument("--artifact_description", type=str, required=True)
     args = parser.parse_args()
-
     go(args)
